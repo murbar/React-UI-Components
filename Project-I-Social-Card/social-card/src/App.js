b4525eba31e1cc4dcd5adfcfe3d1289afe0aa496
Joel Bartlett <joel@murbar.com>
2019-02-11 18:43:46 -0500

    Add footer import

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
 import CardContainer from './components/CardComponents/CardContainer';
+import Footer from './components/FooterComponents/Footer';
 
 const App = () => {
   return (
     <div className="social-card">
       <HeaderContainer />
       <CardContainer />
+      <Footer retweetsCount="6" likesCount="4" />
     </div>
   );
 };
