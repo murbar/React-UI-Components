be5d152f05e1b2be852b80301b0e172c04581e37
Joel Bartlett <joel@murbar.com>
2019-02-11 16:31:35 -0500

    Fill out header components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -2,12 +2,18 @@ import React from 'react';
 import './Header.css';
 import HeaderTitle from './HeaderTitle';
 import HeaderContent from './HeaderContent';
+import ImageThumbnail from './ImageThumbnail';
 
 function HeaderContainer() {
   return (
-    <div className="HeaderContainer">
-      <HeaderTitle />
-      <HeaderContent />
+    <div className="header-container">
+      <div className="header-thumbnail">
+        <ImageThumbnail />
+      </div>
+      <div className="header-main">
+        <HeaderTitle />
+        <HeaderContent />
+      </div>
     </div>
   );
 }
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -2,7 +2,13 @@ import React from 'react';
 import './Header.css';
 
 function HeaderTitle() {
-  return <div>Header title</div>;
+  return (
+    <div className="header-title">
+      <span className="header-name">Lambda School</span>
+      <span className="header-handle">@LambdaSchool</span>
+      <span className="header-timestamp">26 Jan</span>
+    </div>
+  );
 }
 
 export default HeaderTitle;
