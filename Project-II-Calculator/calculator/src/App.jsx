cec254163ac16b92f30a40fefe4324ab04045ce4
Joel Bartlett <joel@murbar.com>
2019-02-12 18:08:46 -0500

    Add more handlers

--- Project-II-Calculator/calculator/src/App.jsx
+++ Project-II-Calculator/calculator/src/App.jsx
@@ -8,9 +8,11 @@ import ActionButton from './components/ButtonComponents/ActionButton';
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { result: 0 };
+    this.state = { result: 0, operand: '', operator: '' };
     this.handleNumberPress = this.handleNumberPress.bind(this);
     this.handleClearPress = this.handleClearPress.bind(this);
+    this.handleOperatorPress = this.handleOperatorPress.bind(this);
+    this.handleEqualsPress = this.handleEqualsPress.bind(this);
   }
 
   handleNumberPress(e, value) {
@@ -25,35 +27,45 @@ export default class App extends React.Component {
     this.setState({ result: 0 });
   }
 
+  handleOperatorPress(e, operator) {
+    if (this.state.result === 0) return;
+    this.setState(state => ({ operand: state.result, operator: operator }));
+  }
+
+  handleEqualsPress(e) {
+    if (this.state.result === 0) return;
+    // display result
+  }
+
   render() {
     return (
       <div className="calculator-container">
         <CalculatorDisplay result={this.state.result} />
         <ButtonRow>
           <ActionButton value="clear" buttonStyle="button-clear" onPress={this.handleClearPress} />
-          <ActionButton value="÷" buttonStyle="button-action" />
+          <ActionButton value="÷" buttonStyle="button-action" onPress={this.handleOperatorPress} />
         </ButtonRow>
         <ButtonRow>
           <NumberButton value="7" buttonStyle="button-number" onPress={this.handleNumberPress} />
           <NumberButton value="8" buttonStyle="button-number" onPress={this.handleNumberPress} />
           <NumberButton value="9" buttonStyle="button-number" onPress={this.handleNumberPress} />
-          <ActionButton value="×" buttonStyle="button-action" onPress={this.handleNumberPress} />
+          <ActionButton value="×" buttonStyle="button-action" onPress={this.handleOperatorPress} />
         </ButtonRow>
         <ButtonRow>
           <NumberButton value="4" buttonStyle="button-number" onPress={this.handleNumberPress} />
           <NumberButton value="5" buttonStyle="button-number" onPress={this.handleNumberPress} />
           <NumberButton value="6" buttonStyle="button-number" onPress={this.handleNumberPress} />
-          <ActionButton value="–" buttonStyle="button-action" />
+          <ActionButton value="–" buttonStyle="button-action" onPress={this.handleOperatorPress} />
         </ButtonRow>
         <ButtonRow>
           <NumberButton value="1" buttonStyle="button-number" onPress={this.handleNumberPress} />
           <NumberButton value="2" buttonStyle="button-number" onPress={this.handleNumberPress} />
           <NumberButton value="3" buttonStyle="button-number" onPress={this.handleNumberPress} />
-          <ActionButton value="+" buttonStyle="button-action" />
+          <ActionButton value="+" buttonStyle="button-action" onPress={this.handleOperatorPress} />
         </ButtonRow>
         <ButtonRow>
           <NumberButton value="0" buttonStyle="button-zero" onPress={this.handleNumberPress} />
-          <ActionButton value="=" buttonStyle="button-action" />
+          <ActionButton value="=" buttonStyle="button-action" onPress={this.handleEqualsPress} />
         </ButtonRow>
       </div>
     );
